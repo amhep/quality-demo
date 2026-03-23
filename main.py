e8a13a657581ba18d39c5d09b70fd0b45bb95a3d
Ammon <ammon.hepworth@gmail.com>
2026-03-23 10:44:49 -0400

    Made palindrome function

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
-name = "Tom"
-print(f"Hello {name}")
+def is_palindrome(s):
+    s = s.lower().replace(" ", "")
+    return s == s[::-1]
+
+def main():
+    test_string = input("Enter a string: ")
+    if is_palindrome(test_string):
+        print(f'"{test_string}" is a palindrome')
+    else:
+        print(f'"{test_string}" is not a palindrome')
+
+if __name__ == "__main__":
+    main()
+
